f8a4dc70499d3f1a14aac753a623381fdcc52d53
rukhulkirom <muhamadrukhulkirom@gmail.com>
2025-04-30 10:36:08 +0700

    Image Gallery

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuIcon = document.querySelector(".menu-icon");
 const backdrop = document.querySelector(".backdrop");
 const navLinks = document.querySelector(".nav-links");
 const closeIcon = document.querySelector(".close-icon");
+const mainImages = document.querySelectorAll(".default .main-img img");
+const thumbnails = document.querySelectorAll(".default .thumb-list div");
 
 menuIcon.addEventListener("click", () => {
   navLinks.classList.add("active");
@@ -16,4 +18,23 @@ closeIcon.addEventListener("click", () => {
 backdrop.addEventListener("click", () => {
   navLinks.classList.remove("active");
   backdrop.classList.remove("active");
+});
+
+const changeImage = (index, mainImages, thumbnails) => {
+  mainImages.forEach((img) => {
+    img.classList.remove("active");
+  });
+
+  thumbnails.forEach((thumb) => {
+    thumb.classList.remove("active");
+  });
+
+  mainImages[index].classList.add("active");
+  thumbnails[index].classList.add("active");
+};
+
+thumbnails.forEach((thumb, index) => {
+  thumb.addEventListener("click", () => {
+    changeImage(index, mainImages, thumbnails);
+  });
 });
